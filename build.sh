442b5fa974be840dd26b29e9d4ee295545dc1dba
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-02 20:45:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,4 @@ then
     rm CMakeFiles/ -rf cmake_install.cmake Makefile lexer
 fi
 cmake .. && make
-if [ -f ./lexer ]
-then
-    echo -e "\e[1;32m${bold}Built succesfully.\e[0m"
-    chmod u+w+x ./lexer && ./lexer
-else
-    echo -e "\e[1;31m${bold}Errors occured, See output above !!\e[0m";
-fi
+
