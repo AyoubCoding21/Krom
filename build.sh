fc72c2855de434c23a627dd8aa93075aa563eb32
AyoubCoding21 <ayoubprogramming95@gmail.com>
2023-08-02 20:33:55 +0200

    File updated

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-cd build
-rm CMakeFiles/ -rf cmake_install.cmake Makefile lexer
+if [ -d build ]
+then
+    cd build
+else
+    mkdir build && cd build
+fi
+if [ -d CMakeFiles/ ]
+then
+    rm CMakeFiles/ -rf cmake_install.cmake Makefile lexer
+fi
 cmake .. && make
 if [ -f ./lexer ]
 then
