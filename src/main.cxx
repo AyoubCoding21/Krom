a4b9e3198cb563a38378a1c3ce0da0874ba29fe9
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-02 21:23:01 +0200

    Update main.cxx

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,13 @@
+// Including libraries
 #include <iostream>
 #include "../lib/lexer.h"
 
 int main() {
+    // Source code input
     std::string input = "let a = 5;\nlet b = 9;\nif (a < b)\n{\nlet c =5;\n}\n";
+    // Transform it into a char vector
     std::vector<char> input_chars(input.begin(), input.end());
+    // Start the Lexer with the input
     lexer::Lexer l(input_chars);
     l.read_char();
     while (true) {
@@ -11,10 +15,11 @@ int main() {
         if (token == Token::EOF_) {
             break;
         } else {
-            std::cout << static_cast<int>(token) << std::endl;
+            std::cout << "Token converted to int : " << static_cast<int>(token) << std::endl;
         }
     }
-    std::cout << l.get_ch() << " " << l.get_position() << " " << l.get_read_position() << std::endl;
+    // Printing last character
+    std::cout << "Character : " << l.get_ch() << " It's position : " << l.get_position() << " It's read position : " << l.get_read_position() << std::endl;
 
     return 0;
 }
